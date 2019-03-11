104a169da3d60beb3ab4c7d3dde6959c96c1b249
Gints Murans <gm@gm.lv>
2019-03-11 10:05:16 +0200

    Update to ES6

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # JMT - Javascript Micro Templating
 
-A repository for John Resig's javascript micro templating script for various package managers
+An ES6 version of John Resig's javascript micro templating script.
+
+`new Function()` is diallowed in strict mode, but for now I cannot find another way to do the same functionality.
 
 # Installation
+
+To install via npm run: `npm install jmt.js`
 
 To install via composer run: `composer require 4apps/jmt`
--- jmt.js
+++ jmt.js
@@ -1,35 +1,71 @@
 // Simple JavaScript Templating
+// Gints Murans, https://github.com/gintsmurans/jmt
 // John Resig - http://ejohn.org/ - MIT Licensed
-(function(){
-  var cache = {};
 
-  this.tmpl = function tmpl(str, data){
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
-      cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
+export default class Jmt {
+    constructor(str, ES6 = false) {
+        if (('cache' in Jmt) === false) {
+            Jmt.cache = {};
+        }
 
-      // Generate a reusable function that will serve as a template
-      // generator (and which will be cached).
-      new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+        if (/\W/.test(str)) {
+            this.templateStr = str;
+        } else {
+            this.templateStr = document.getElementById(str).innerHTML;
+        }
 
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
+        if (ES6) { // Lets wait for IE 11 support drop
+            // eslint-disable-next-line no-new-func
+            this.fn = new Function(
+                'templateData',
+                `var p=[],print=function(){p.push.apply(p,arguments);};
 
-        // Convert the template into pure JavaScript
-        str
-          .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'")
-      + "');}return p.join('');");
+                // Introduce the data as local variables using with(){}
+                with(templateData){
+                    // Convert the template into pure JavaScript
+                    p.push(\`${this.templateStr
+                        .split('<%')
+                            .join('\t')
+                        .replace(/((^|%>)[^\t]*)'/g, '$1\r')
+                        .replace(/\t=(.*?)%>/g, '${$1}')
+                        .split('\t')
+                            .join('`);')
+                        .split('%>')
+                            .join('p.push(`')
+                        .split('\r')
+                            .join('\\`')}\`);
+                }
+                return p.join('');`
+            );
+        } else {
+            // eslint-disable-next-line no-new-func
+            this.fn = new Function(
+                'templateData',
+                `var p=[],print=function(){p.push.apply(p,arguments);};
 
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-  };
-})();
+                // Introduce the data as local variables using with(){}
+                with(templateData){
+                    // Convert the template into pure JavaScript
+                    p.push('${this.templateStr
+                        .replace(/[\r\t\n]/g, " ")
+                        .split('<%')
+                            .join('\t')
+                        .replace(/((^|%>)[^\t]*)'/g, '$1\r')
+                        .replace(/\t=(.*?)%>/g, '\',$1,\'')
+                        .split('\t')
+                            .join('\');')
+                        .split('%>')
+                            .join('p.push(\'')
+                        .split('\r')
+                            .join('\\\'')}');
+                }
+                return p.join('');`
+            );
+        }
+    }
+
+    html(data) {
+        const templateData = (data == null ? {} : data);
+        return this.fn(templateData);
+    }
+}
